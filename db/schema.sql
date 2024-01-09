baeab3a4cbde4d34b9df480290c56a19726b354a
Joey V <joey03v@gmail.com>
2024-01-09 14:55:15 -0600

    finished the tables in the schema.sql file also added some code to server.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,24 @@ CREATE DATABASE department_db;
 USE department_db;
 
 CREATE TABLE department (
-    
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(100) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
-)
-
-CREATE TABLE employee(
-    
-)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employee_name VARCHAR(100) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
