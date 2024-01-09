f3ab1053dbaf0c201a5671ef6673f0a29c9fb5f7
Joey V <joey03v@gmail.com>
2024-01-09 14:29:02 -0600

    added some empty tables to the schema and also added an inquirer set up

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE department (
+    
+)
+
+CREATE TABLE role (
+
+)
+
+CREATE TABLE employee(
+    
+)
